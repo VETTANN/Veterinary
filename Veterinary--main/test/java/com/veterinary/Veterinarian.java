0826fc884fa7363039610ecd7fa89c7090b34feb
yoongiia <dvorakivska0402@gmail.com>
2026-05-07 00:54:50 +0300

    Add CI configuration with Checkstyle and JaCoCo

---
+++ Veterinary--main/test/java/com/veterinary/Veterinarian.java
@@ -0,0 +1,28 @@
+package com.veterinary;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "veterinarian")
+public class Veterinarian {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vet_id")
+    private Integer vetId;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "experience_years")
+    private Integer experienceYears;
+
+    public Veterinarian() {}
+
+    public Veterinarian(String fullName, Integer experienceYears) {
+        this.fullName = fullName;
+        this.experienceYears = experienceYears;
+    }
+
+    public String getFullName() { return fullName; }
+    public Integer getExperienceYears() { return experienceYears; }
+}
